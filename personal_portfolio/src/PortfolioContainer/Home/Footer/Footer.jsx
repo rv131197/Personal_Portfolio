4dd63aa15e4dfacd64bb0b4014b27f317635b175
Ramya V <ramya@Ramyas-MacBook-Air.local>
2022-01-01 14:47:13 +0530

    Added Footer component

---
+++ personal_portfolio/src/PortfolioContainer/Home/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <div className="footer-container">
+            <footer className="footer-parent">
+                <img src={require ('../../../assets/Home/shape-bg.png')} alt='internet connection error' />
+
+            </footer>
+        </div>
+    )
+}
